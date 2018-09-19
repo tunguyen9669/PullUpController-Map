f2b373abb4a756d3e5346b6c85456fae9a18bcc6
TuNguyen <tunguyen090896@gmail.com>
2018-09-19 12:07:23 +0700

    Initial Commit

---
+++ SuperMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperMap
+//
+//  Created by admin on 9/19/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
