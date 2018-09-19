7f58676e11c840fecff81065fb455bbb65957fbb
TuNguyen <tunguyen090896@gmail.com>
2018-09-19 13:50:42 +0700

    pull up controller

---
+++ SuperMap/Search/SearchResultCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchResultCell.swift
+//  SuperMap
+//
+//  Created by admin on 9/19/18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
