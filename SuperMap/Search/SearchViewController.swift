8801790def15ab708c9806e31bb681cce2dcaa0f
TuNguyen <tunguyen090896@gmail.com>
2018-09-21 15:05:12 +0700

    control ui

--- SuperMap/Search/SearchViewController.swift
+++ SuperMap/Search/SearchViewController.swift
@@ -11,30 +11,15 @@ import MapKit
 import PullUpController
 
 class SearchViewController: PullUpController {
-    @IBAction func reduceZoom(_ sender: Any) {
-    }
-    @IBAction func increaseZoom(_ sender: Any) {
-    }
-    @IBAction func direction(_ sender: Any) {
-    }
+    @IBOutlet weak var secondView: UIView!
     
-    @IBOutlet weak var startLocation: UIButton!
-    @IBOutlet weak var endLocation: UIButton!
-    @IBAction func openEndLocation(_ sender: Any) {
-    }
-    
-    @IBAction func openStartLocation(_ sender: Any) {
-    }
-    
-    @IBOutlet weak var separatorSearchView: UIView!{
+    @IBOutlet weak var sepatorView: UIView! {
         didSet {
-            separatorSearchView.layer.cornerRadius = separatorSearchView.frame.height/2
+            sepatorView.layer.cornerRadius = sepatorView.frame.height/2
         }
     }
-    
-//    private var locations = [(title: String, location: CLLocationCoordinate2D)]()
-   
-    
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var menuView: UIView!
     public var portraitSize: CGSize = .zero
     public var landscapeFrame: CGRect = .zero
 
@@ -42,10 +27,11 @@ class SearchViewController: PullUpController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
- 
+        setupView()
+    }
+    func setupView() {
         portraitSize = CGSize(width: min(UIScreen.main.bounds.width, UIScreen.main.bounds.height),
-                              height: 250)
+                              height: secondView.frame.maxY)
         landscapeFrame = CGRect(x: 5, y: 50, width: 280, height: 300)
         
         willMoveToStickyPoint = { point in
@@ -60,32 +46,30 @@ class SearchViewController: PullUpController {
             print("onDrag: \(point)")
         }
     }
-    // MARK: - PullUpController
-
-    override var pullUpControllerPreferredSize: CGSize {
-        return portraitSize
-    }
-
-    override var pullUpControllerPreferredLandscapeFrame: CGRect {
-        return landscapeFrame
-    }
-//
-//    override var pullUpControllerPreviewOffset: CGFloat {
-//        return view.frame.height
-//    }
-//
-//    override var pullUpControllerMiddleStickyPoints: [CGFloat] {
-//        return [view.frame.maxY]
-//    }
-//
-//    override var pullUpControllerIsBouncingEnabled: Bool {
-//        return false
-//    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
         view.layer.cornerRadius = 12
+    }
+    // MARK: - PullUpController
+    override var pullUpControllerPreferredSize: CGSize {
+        return portraitSize
+    }
+    
+    override var pullUpControllerPreferredLandscapeFrame: CGRect {
+        return landscapeFrame
+    }
+    override var pullUpControllerPreviewOffset: CGFloat {
+        return menuView.frame.height
+    }
+    
+    override var pullUpControllerMiddleStickyPoints: [CGFloat] {
+        return [firstView.frame.maxY]
+    }
+    
+    override var pullUpControllerIsBouncingEnabled: Bool {
+        return false
     }
   
 }
